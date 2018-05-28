6fcc9b81c9a4122fa740cfbac65c639d0e3f3b41
kiranbhalerao123 <bhaleraok65@gmail.com>
2018-05-28 08:06:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/kiran/firebasedemo/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.kiran.firebasedemo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    EditText emailText;
+    EditText passText;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    Button signupButton;
+    Button logoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailText = findViewById(R.id.emailText);
+        passText = findViewById(R.id.passText);
+        loginButton = findViewById(R.id.loginButton);
+        signupButton = findViewById(R.id.signupButton);
+        logoutButton = findViewById(R.id.logoutButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (mAuth.getCurrentUser() != null) {
+                    Toast.makeText(MainActivity.this, "Login Successfully "+mAuth.getCurrentUser().getEmail(), Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        logoutButton.setOnClickListener((view) -> {
+            mAuth.signOut();
+            Toast.makeText(MainActivity.this, "logout Successfully", Toast.LENGTH_LONG).show();
+        });
+        signupButton.setOnClickListener((v) -> {
+            signUp(emailText.getText().toString(), passText.getText().toString());
+        });
+        loginButton.setOnClickListener((v) -> {
+            login(emailText.getText().toString(), passText.getText().toString());
+        });
+    }
+
+    private void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Wrong data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void signUp(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Authentication succeed.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+}
